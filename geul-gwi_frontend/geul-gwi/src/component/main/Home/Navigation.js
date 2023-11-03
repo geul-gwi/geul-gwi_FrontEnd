de278591271a24820e6272ba15a4e557f63488bf
sejyom <akxxkd01@gmail.com>
2023-11-03 13:50:25 +0900

    design: modify icon

--- geul-gwi_frontend/geul-gwi/src/component/main/Home/Navigation.js
+++ geul-gwi_frontend/geul-gwi/src/component/main/Home/Navigation.js
@@ -17,7 +17,7 @@ const menus = []
 menus.push({ "name": "홈", "src": "/icon/Navigation/home.svg", "target": "/main" })
 menus.push({ "name": "알림", "src": "/icon/Navigation/bell.svg", "target": "/alarm" })
 menus.push({ "name": "친구", "src": "/icon/Navigation/users.svg", "target": "/friend" })
-menus.push({ "name": "회원 검색", "src": "/icon/Navigation/users.svg", "target": "/member" })
+menus.push({ "name": "회원 검색", "src": "/icon/Navigation/search.png", "target": "/member" })
 menus.push({ "name": "쪽지함", "src": "/icon/Navigation/letter.png", "target": "/main/message" })
 menus.push({ "name": "챌린지", "src": "/icon/Navigation/notepad.png", "target": "/main/WritingChallenge" })
 menus.push({ "name": "작성", "src": "/icon/Navigation/plus.png", "target": "/main/Writing" })
