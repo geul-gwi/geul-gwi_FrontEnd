9ae7ea5ed01ebbc706b86306873814a406cee13e
DESKTOP\iuhea <ctrlme@naver.com>
2023-11-05 17:23:44 +0900

    최종.이었으면 좋겠디.. ~

--- geul-gwi_frontend/geul-gwi/src/component/main/LeftNavi/search/SearchAction.js
+++ geul-gwi_frontend/geul-gwi/src/component/main/LeftNavi/search/SearchAction.js
@@ -223,7 +223,7 @@ const HoveredBack = styled.div`
   width: 100%;
   height: 100%;
   border-radius: 16px;
-  background-color: rgba(255, 255, 255, 0.7);
+  background-color: rgba(255, 255, 255, 0.65);
   z-index: 1;
 
 `;
