35aec03bdbe974280343256db199af39623ff5f1
DESKTOP\iuhea <ctrlme@naver.com>
2023-11-06 23:49:11 +0900

    .

--- geul-gwi_frontend/geul-gwi/src/component/main/Writing/WritingAction.js
+++ geul-gwi_frontend/geul-gwi/src/component/main/Writing/WritingAction.js
@@ -131,7 +131,7 @@ const WritingAction = () => {
                     },
                 });
                 alert("작성이 완료되었습니다.");
-                navigate("/main");
+                navigate("/main/WritingChallenge");
                 
             } else {
                 console.log("진행중인 챌린지가 없습니다.");
